283d5a63518c6b6d0a507a739a7682f6a0202206
Danny Wilson <dannywilson32@gmail.com>
2018-11-08 21:46:35 +0000

    added auth stuff and spotify endpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+class LoginButton extends Component {
+  render() {
+    return (
+      <div className="LoginButton">
+        <a href="https://accounts.spotify.com/authorize?client_id=b6f4e429c5484a9c9f773c53c0627d7b&redirect_uri=http:%2F%2Flocalhost:3000%2F&scope=user-top-read&response_type=token&state=123">Login</a>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+  getAccessToken(hash){
+    console.log(hash)
+    if (hash === "")
+      return null
+    else
+      return hash.split('&')[0].split('=')[1]
+  }
+
+  getBandsOn(festival){
+    if (festival!=='glasto')
+      console.log("we're only doing glasto right now")
+    return [
+      'Fleetwood Mac',
+      'Eminem',
+      'Blur',
+      'Radiohead',
+      'Kendrick Lamar'
+    ];
+  }
+
+  getUserTopArtists(accessToken){
+    axios({
+      'method': 'get',
+      'url':'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term',
+      'headers': { 'Authorization': 'Bearer '+ accessToken },
+    })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+
+  componentDidMount(){
+    const accessToken = this.getAccessToken(window.location.hash)
+
+    if (accessToken != null){
+      const bandson = this.getBandsOn('glasto')
+      const userTopArtists = this.getUserTopArtists(accessToken)
+
+      console.log(bandson, userTopArtists)
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <LoginButton />
         </header>
       </div>
     );
