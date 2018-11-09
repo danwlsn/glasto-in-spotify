633ac3f7217091e11fe822f467ba3538e4c5a049
Danny Wilson <dannywilson32@gmail.com>
2018-11-09 16:12:28 +0000

    added bandlist and cleaned up some stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,44 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+class Band extends Component {
+  render(){
+    return (
+      <div className="Band">
+        { this.props.name }
+      </div>
+    )
+  }
+}
+
+class BandList extends Component {
+  render() {
+    // const bandList = this.props.bandList.maps(band => {
+    //   return <Band name={band} />
+    // })
+    return (
+      <div className="MatchingButton">
+        <h2>{this.props.title}</h2>
+        <ul>
+          {this.props.bandList}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class FindMatchingButton extends Component {
+  render() {
+    return (
+      <div className="MatchingButton">
+        <button disabled={this.props.disabled} onClick={this.props.click}>
+          Find matching
+        </button>
+      </div>
+    );
+  }
+}
+
 class LoginButton extends Component {
   render() {
     return (
@@ -14,6 +52,19 @@ class LoginButton extends Component {
 }
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      'authed': false,
+      'accessToken': null,
+      'festivalBandList': this.getBandsOn(),
+      'userTopBands': null,
+      'matchingBands': null
+    }
+
+    this.findMatchingButtonOnClick = this.findMatchingButtonOnClick.bind(this);
+  }
+
   getAccessToken(hash){
     console.log(hash)
     if (hash === "")
@@ -34,18 +85,52 @@ class App extends Component {
     ];
   }
 
+  findMatchingButtonOnClick(){
+    const festivalBands = this.state.festivalBandList
+    const userTopBands = this.state.userTopBands
+
+    console.log(this.findMatchingBands(festivalBands, userTopBands))
+  }
+
+  findMatchingBands(festivalBands, userBands){
+    let matching = []
+
+    for(let band in festivalBands) {
+      if(userBands.indexOf(festivalBands[band]) > -1){
+          matching.push(festivalBands[band]);
+      }
+    }
+
+    this.setState((state, props) => {
+      return {'matchingBands': matching }
+    })
+    return matching
+  }
+
+  mapApiDataToBandList(data){
+    return data.items.map(item => item.name)
+  }
+
   getUserTopArtists(accessToken){
+    const _this = this
+
     axios({
       'method': 'get',
       'url':'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term',
       'headers': { 'Authorization': 'Bearer '+ accessToken },
     })
       .then(function (response) {
-        console.log(response.data);
+        // TODO enable button because we're ready
+        const userTopBands = _this.mapApiDataToBandList(response.data)
+
+        _this.setState((state, props) => {
+          return {'userTopBands': userTopBands }
+        })
+        console.log(_this.state)
       })
       .catch(function (error) {
         // handle error
-        console.log(error);
+        console.log(error)
       });
   }
 
@@ -53,10 +138,12 @@ class App extends Component {
     const accessToken = this.getAccessToken(window.location.hash)
 
     if (accessToken != null){
-      const bandson = this.getBandsOn('glasto')
-      const userTopArtists = this.getUserTopArtists(accessToken)
+      this.getUserTopArtists(accessToken)
+      this.setState((state, props) => {
+        return {'authed': true, 'accessToken': accessToken }
+      })
 
-      console.log(bandson, userTopArtists)
+      console.log(this.state)
 
     }
   }
@@ -66,7 +153,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <LoginButton />
+            <FindMatchingButton disabled={!this.state.authed} click={this.findMatchingButtonOnClick}/>
         </header>
+        <BandList bandList={this.state.festivalBandList} />
+        <BandList bandList={this.state.userTopBands} />
+        <BandList bandList={this.state.matchingBands} />
       </div>
     );
   }
